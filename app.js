3d4e2d744a157ddc8988c8e14d2a47ae55c1f60f
Zasmooncat <zasmomoxipol@gmail.com>
2024-09-27 19:12:34 +0000

    domains

---
+++ app.js
@@ -0,0 +1,16 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extension = ['.com', '.net', '.es', '.zas'];
+
+
+for (let pro of pronoun){
+ for (let ad of adj){
+  for (let no of noun){
+    for (let ext of extension)
+
+        console.log(pro + ad + no + ext);
+
+    }
+    }
+    }
