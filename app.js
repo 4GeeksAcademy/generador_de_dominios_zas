da85e83bb050363674413b5aadfaaf925fc7aea4
Zasmooncat <zasmomoxipol@gmail.com>
2024-09-27 19:17:15 +0000

    domainss

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let extension = ['.com', '.net', '.es', '.zas'];
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".com", ".net", ".es", ".zas"];
 
-
-for (let pro of pronoun){
- for (let ad of adj){
-  for (let no of noun){
-    for (let ext of extension)
-
-        console.log(pro + ad + no + ext);
-
+for (let pro of pronoun) {
+  for (let ad of adj) {
+    for (let no of noun) {
+      for (let ext of extension) console.log(pro + ad + no + ext);
     }
-    }
-    }
+  }
+}
