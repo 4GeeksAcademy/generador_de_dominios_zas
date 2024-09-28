78a4f11b6be0267ac5cdfc28034c69e5cd18d8a4
Zasmooncat <zasmomoxipol@gmail.com>
2024-09-28 09:08:46 +0000

    conforeach

--- app.js
+++ app.js
@@ -3,10 +3,31 @@ let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 let extension = [".com", ".net", ".es", ".zas"];
 
-for (let pro of pronoun) {
+
+// FOR OF
+
+/*for (let pro of pronoun) {
   for (let ad of adj) {
     for (let no of noun) {
       for (let ext of extension) console.log(pro + ad + no + ext);
     }
   }
-}
+}*/
+
+
+
+// FOR EACH
+
+let domains = [];
+
+pronoun.forEach(pro=>{
+  adj.forEach(a=>{
+    noun.forEach(no=>{
+      extension.forEach(ext=>{
+        domains.push(pro+a+no+ext);
+     });
+    });
+  });
+});
+
+domains.forEach(domain=>{console.log(domain)});
